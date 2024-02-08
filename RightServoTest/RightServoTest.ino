3fc09085bf3d4c9717f9fe88d176665af6f2b5b5
Abdul Amin <78924070+iamabdulamin123@users.noreply.github.com>
2024-02-08 12:57:52 +0000

    Update RightServoTest.ino

--- RightServoTest/RightServoTest.ino
+++ RightServoTest/RightServoTest.ino
@@ -4,12 +4,6 @@ Servo servoRight;                            // Declare right servo
  
 void setup()                                 // Built in initialization block
 {
-  Serial.begin(9600);
-  Serial.println("Sketch Starting");  
-
-  tone(4, 3000, 1000);                       // Play tone for 1 second
-  delay(1000);
-
   servoRight.attach(12);                     // Attach right signal to pin 12
 
   servoRight.writeMicroseconds(1300);        // Right wheel clockwise
